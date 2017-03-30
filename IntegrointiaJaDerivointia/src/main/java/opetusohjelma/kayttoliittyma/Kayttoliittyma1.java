169a4534d19ec8cb1763c6c698ddff1c61f07743
Kätkä Iisa M <iisa.katka@cs.helsinki.fi>
2017-03-30 16:20:28 +0300

    Vaihtoehtoisten käyttöliittymien kokeilu

--- IntegrointiaJaDerivointia/src/main/java/opetusohjelma/kayttoliittyma/Kayttoliittyma1.java
+++ IntegrointiaJaDerivointia/src/main/java/opetusohjelma/kayttoliittyma/Kayttoliittyma1.java
@@ -19,8 +19,8 @@ public class Kayttoliittyma1 implements Runnable {
 
     @Override
     public void run() {
-        frame = new JFrame("Integrointia ja derivointia");
-        frame.setPreferredSize(new Dimension(400, 100));
+        frame = new JFrame("Integration and derivation");
+        frame.setPreferredSize(new Dimension(600, 150));
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -34,10 +34,14 @@ public class Kayttoliittyma1 implements Runnable {
         GridLayout layout = new GridLayout(1, 3);
         container.setLayout(layout);
 
-        JTextArea textArea = new JTextArea("\n Valitse funktion \n tai pyydä konetta \n arpomaan sen");
-        JButton arvoNappi = new JButton("Arpominen");
-        JButton valitseNappi = new JButton("Valitseminen");
-
+        JTextArea textArea = new JTextArea("\n Choose a function \n or ask the computer \n to draw lots to decide it");
+        JButton arvoNappi = new JButton("Computer \n chooses");
+        JButton valitseNappi = new JButton("You choose");
+        Valinnan1Kuuntelija valitsija = new Valinnan1Kuuntelija();
+        
+        arvoNappi.addActionListener(valitsija);
+        valitseNappi.addActionListener(valitsija);
+        
         container.add(arvoNappi);
         container.add(textArea);
         container.add(valitseNappi);
--- IntegrointiaJaDerivointia/src/main/java/opetusohjelma/kayttoliittyma/Valinnan1Kuuntelija.java
+++ IntegrointiaJaDerivointia/src/main/java/opetusohjelma/kayttoliittyma/Valinnan1Kuuntelija.java
@@ -7,11 +7,15 @@ import java.awt.event.ActionListener;
 public class Valinnan1Kuuntelija implements ActionListener {
     private String valinta;
     
+    public Valinnan1Kuuntelija() {
+        
+    }
+    
     
 
     @Override
     public void actionPerformed(ActionEvent e) {
-       
+        System.out.println("koe");
     }
     
     
