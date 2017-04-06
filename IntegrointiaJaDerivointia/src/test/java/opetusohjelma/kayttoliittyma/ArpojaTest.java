020be356327f5f595048ed31d8c31006ade9a44a
mustikka2015 <iisa.katka@helsinki.fi>
2017-04-06 18:03:24 +0300

    deadline 4

--- IntegrointiaJaDerivointia/src/test/java/opetusohjelma/kayttoliittyma/ArpojaTest.java
+++ IntegrointiaJaDerivointia/src/test/java/opetusohjelma/kayttoliittyma/ArpojaTest.java
@@ -1,7 +1,71 @@
 
 package opetusohjelma.kayttoliittyma;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
 
 public class ArpojaTest {
     
+    public ArpojaTest() {
+    
+}
+    
+    @Test
+    public void eksponenttiArvottuOikeinTest1() {
+        Arpoja arpoja = new Arpoja();
+        int eksp = arpoja.arvoPolynominEksponentti();
+        assertTrue(-10 <= eksp && eksp <=10);
+    }
+    
+    @Test
+    public void eksponenttiArvottuOikeinTest2() {
+        Arpoja arpoja = new Arpoja();
+        int eksp = arpoja.arvoPolynominEksponentti();
+        assertFalse(eksp > 10);
+    }
+    
+    @Test
+    public void eksponenttiArvottuOikeinTest3() {
+        Arpoja arpoja = new Arpoja();
+        int eksp = arpoja.arvoPolynominEksponentti();
+        assertFalse(eksp < -10);
+    }
+    
+    @Test
+    public void funktioArvottuOikeinTest() {
+        Arpoja arpoja = new Arpoja();
+        String funktio = arpoja.arvoFunktio();
+        assertTrue(funktio.equals("polynom") || funktio.equals("sin")|| funktio.equals("cos"));
+    }
+    
+    @Test
+    public void toimintoArvottuOikeinTest() {
+        Arpoja arpoja = new Arpoja();
+        String toiminto = arpoja.arvoToiminto();
+        assertTrue(toiminto.equals("Integroi")|| toiminto.equals("Derivoi"));
+    }
+    
+    @Test
+    public void kerroinArvottuOikeinTest1() {
+        Arpoja arpoja = new Arpoja();
+        double kerroin = arpoja.arvoKerroin();
+        assertTrue(-10.0 <= kerroin && kerroin <=10.0);
+    }
+    
+    @Test
+    public void kerroinArvottuOikeinTest2() {
+        Arpoja arpoja = new Arpoja();
+        double kerroin = arpoja.arvoKerroin();
+        assertFalse(kerroin < -10.0);
+    }
+    
+    @Test
+    public void kerroinArvottuOikeinTest3() {
+        Arpoja arpoja = new Arpoja();
+        double kerroin = arpoja.arvoKerroin();
+        assertFalse(10.0 < kerroin);
+    }
 }
